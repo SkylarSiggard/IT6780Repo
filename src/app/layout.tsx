0244ee59df9bbd64c66da0c994075c45514c6cb6
SkylarSiggard <siggardskylar@gmail.com>
2024-09-05 20:39:29 -0600

    added cat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Nav from "@/app/components/Nav";
 
 export const metadata: Metadata = {
   title: "The Recipe Hub",
@@ -26,10 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="min-h-screen">
+      <Nav/>
+      <div className="flex justify-center items-center p-24">
         {children}
+      </div>
       </body>
     </html>
   );
